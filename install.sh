1915dc240284c7782a6630b3bd8134dba54be0cd
myml <wurongjie1@gmail.com>
2022-08-05 14:19:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
 mkdir -p ~/.config/git/hooks
 wget -N -P ~/.config/git/hooks https://raw.githubusercontent.com/myml/git-email-check/master/pre-commit
+chmod +x ~/.config/git/hooks/pre-commit
 git config --global core.hooksPath ~/.config/git/hooks
